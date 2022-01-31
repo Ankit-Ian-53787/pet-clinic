49ba2db03a23220fbd46640cd9520e988aa625ff
Ankit <ankitgup64@gmail.com>
2022-01-31 19:13:55 +0530

    Adding POJOs. Closing #2

---
+++ src/main/java/petclinic/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package petclinic.springframework.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/petclinic/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package petclinic.springframework.petclinic.model;
+
+public class Vet extends Person{
+}
