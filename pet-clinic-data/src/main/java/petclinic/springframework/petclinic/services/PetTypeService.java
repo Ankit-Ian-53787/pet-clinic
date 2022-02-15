b02cce6aa11f69a15e1e0e409720b8a3d7524d39
Ankit <ankitgup64@gmail.com>
2022-02-15 14:17:05 +0530

    created pet type map service. closes #18

---
+++ pet-clinic-data/src/main/java/petclinic/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package petclinic.springframework.petclinic.services;
+
+import petclinic.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+}
